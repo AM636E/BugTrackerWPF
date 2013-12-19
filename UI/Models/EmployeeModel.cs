b990fae398958e378065ab307c0a463d97797243
Nazariy <bnazariy@gmail.com>
2013-12-19 19:31:25 +0200

    Employee entity

--- UI/Models/EmployeeModel.cs
+++ UI/Models/EmployeeModel.cs
@@ -1,13 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using UI.Entities;
 
 namespace UI.Models
 {
     class EmployeeModel
     {
+        ObservableCollection<EmployeeEntity> _entities;
 
+        public ObservableCollection<EmployeeEntity> Entities
+        {
+            get { return _entities; }
+        }
+
+        public EmployeeModel()
+        {
+            _entities = new ObservableCollection<EmployeeEntity>();
+        }
+
+        public void Load()
+        {
+            _entities = DAL.Manager.SelectFromTable("Employee", String.Empty, "*").ToEmployeers();
+        }
     }
 }
