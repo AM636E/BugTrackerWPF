68ef4daedd201da0d06332bd98ca00b619e2d5d3
Nazariy <bnazariy@gmail.com>
2013-12-19 19:33:20 +0200

    Project Model

---
+++ UI/Models/ProjectModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UI.Entities;
+
+namespace UI.Models
+{
+    class ProjectModel
+    {
+        ObservableCollection<ProjectEntity> _entities;
+
+        public ObservableCollection<ProjectEntity> Entities
+        {
+            get { return _entities; }
+        }
+
+        public ProjectModel()
+        {
+            _entities = new ObservableCollection<ProjectEntity>();
+        }
+
+        public void Load()
+        {
+            _entities = DAL.Manager.SelectFromTable("Project", String.Empty, "*").ToProjects();
+        }
+    }
+}
