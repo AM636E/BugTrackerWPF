7839f159b563b9d21c14c892944abed0f89bfd35
Nazariy <bnazariy@gmail.com>
2013-12-21 17:31:19 +0200

    filterviewmodel

---
+++ UI/ViewModel/BugTitleFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.ViewModel
+{
+    public interface IFilter
+    {
+        string FilterTerm{get; private set;}
+        void Apply();
+        void Remove();
+    }
+
+    class BugTitleFilter : IFilter
+    {
+        private string _filterTerm;
+
+        public string FilterTerm { get { return _filterTerm; } set { _filterTerm = value; } }
+
+        public BugTitleFilter()
+        {
+
+        }
+    }
+}
--- UI/ViewModel/FilterViewModel.cs
+++ UI/ViewModel/FilterViewModel.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections;
+
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace UI.ViewModel
 {
     class FilterViewModel
     {
+        ICollection _items;
+        public FilterViewModel(ICollection items)
+        {
+            _items = items;
+        }
+
+        public RelayCommand<String> ApplyFilter
+        { get; set; }
+
+        public RelayCommand RemoveFilter { get; set; }
+
+        private void Apply(object term)
+        {
+
+        }
     }
 }
