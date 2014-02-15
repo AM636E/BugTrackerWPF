1783c2b0a82ea4f54d28c24c908a214c92fa59f1
Nazariy <bnazariy@gmail.com>
2014-02-15 14:21:16 +0200

    WCF made working

---
+++ BugAccessLibrary/BugAccessService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace BugAccessLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class BugAccessService : IBugAccessService
+    {
+        static int elementsSend = 0;
+        public BugEntity[] GetBugs()
+        {
+            SqlConnectionStringBuilder con = new SqlConnectionStringBuilder();
+            con.DataSource = @"DRUD\ZAZSQLSERVER";
+            con.IntegratedSecurity = true;
+            con.InitialCatalog = "Zaz";
+            con.UserID = "zaz";
+
+            SqlConnection sql = new SqlConnection(con.ToString());
+            SqlCommand query = sql.CreateCommand();
+            query.CommandText = "SELECT * FROM Bug";
+
+            SqlDataAdapter da = new SqlDataAdapter(query);
+
+            DataSet ds = new DataSet();
+            
+            da.Fill(ds);
+
+            var a = ds.ToBugs();
+            //a.RemoveRange(, 300);
+                return a.ToArray();
+        }
+    }
+
+    public static class DBExtensions
+    {
+        public static List<BugEntity> ToBugs(this DataSet ds)
+        {
+            var rows = ds.Tables[0].Rows;
+            List<BugEntity> result = new List<BugEntity>(rows.Count);
+
+            foreach (DataRow row in rows)
+            {
+                try
+                {
+                    int id = Convert.ToInt32(row["bugid"]);
+                    int a = Convert.ToInt32(row["bugprojectid"]);
+                    string b = row["bugtitle"].ToString();
+                    string c = row["bugsummary"].ToString();
+                    int d = Convert.ToInt32(row["bugreporterid"].ToString());
+                    int e = Convert.ToInt32(row["bugfixerid"].ToString());
+                  
+                    DateTime n = (DateTime)row["bugsubmitted"];//.GetType();
+                   
+                    result.Add(new BugEntity(
+                            id, b, c
+                    ));
+                }
+                catch (Exception e)
+                {
+                    result.Add(new BugEntity() { Title = e.Message });
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- test/Program.cs
+++ test/Program.cs
@@ -8,81 +8,27 @@ using System.Threading;
 using BugTrackerWPF.DAL;
 using BugTrackerWPF;
 using UI.Entities;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Sql;
 
 namespace test
 {
-    static class DAL
+    using DataAccess;
+    public class Program
     {
-        private static DBManager _manager = OracleDB.Instance;
-        public static DBManager Manager
+        public static void Main()
         {
-            get
+            BugAccessServiceClient client = new BugAccessServiceClient();
+
+            var a = client.GetBugs();
+
+            foreach(var i in a)
             {
-                return _manager;
+                Console.WriteLine(i.Title);
             }
-        }
-    }
-    class Program
-    {
-        class Test
-        {
-            private DateTime _created = DateTime.Now;
 
-            public DateTime Created
-            {
-                get { return _created; }
-            }
-        }
-
-        public static Logger log;
-
-
-
-        static void Main(string[] args)
-        {
-            BugEntity bug = new BugEntity(100);
-           
-            string query = @"
-INSERT INTO BUG
-(
-    BUGPROJECTID,
-    BUGTITLE,
-    BUGSUMMARY,
-    BUGSUBMITTED,
-    BUGREPORTERID,
-    BUGFIXERID,
-    BUGPRIORITY,
-    BUGSEVERITYID,
-    BUGCOMPONENTID,
-    BUGBUILD,
-    BUGSTATUSID
-)
-VALUES (";
-            //to_timestamp('17-DEC-13 11.53.46.053000000 AM','DD-MON-RR HH.MI.SSXFF AM')
-            Console.WriteLine(bug.Project.Id.ToString());
-            query += bug.Project.Id.ToString() + ",\'";
-            query += bug.Title + "\',";
-            query += '\'' + bug.Summary + "\',";
-            query += @"to_timestamp( '" + bug.Created.ToString("dd-MM-yyyy HH.m.s") + @"', 'DD-MM-RRRR HH24.MI.SS'),";
-            query += bug.Repoter.Id.ToString() + ',';
-            query += bug.Fixer.Id.ToString() + ',';
-            query += ((int)bug.Prioriry).ToString() + ',';
-            query += ((int)bug.Severity).ToString() + ',';
-            query += ((int)bug.Component).ToString() + ',';
-            query += ((int)bug.Build).ToString() + ',';
-            query += ((int)bug.Status).ToString() + ")";
-
-
-            try
-            {Console.WriteLine(query);
-                DAL.Manager.ExecuteQuery(query);
-                DAL.Manager.ExecuteQuery("COMMIT");
-                
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            client.Close();
         }
     }
 }
